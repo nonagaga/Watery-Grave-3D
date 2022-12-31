039ed608704914717acb64bf7f74a4beae50fb13
nonagaga <gabrielreyeswagner@gmail.com>
2022-12-31 14:10:29 -0500

    collision layers + goofy ahh turn fish

--- Scenes/Enemies/Enemy.gd
+++ Scenes/Enemies/Enemy.gd
@@ -2,11 +2,19 @@ extends KinematicBody
 
 onready var rays = $"Rays"
 
-export var speed = 200
+export var speed = 5
+
+export var turn_speed = 2
 
 onready var target = $"/root/SceneManager/GameScene/Character Flashlight/Character"
 
 func _physics_process(delta):
-	var direction = global_translation.direction_to(target.global_translation)
-	look_at(target.global_translation, Vector3.UP)
-	move_and_slide(direction*speed*delta, Vector3.UP)
+	turn(delta)
+	move(delta)
+
+func turn(delta):
+	var rotation = transform.looking_at(target.global_translation, Vector3.UP).basis.get_rotation_quat()
+	transform.basis = self.transform.basis.slerp(Basis(rotation), delta * turn_speed).orthonormalized()
+
+func move(delta):
+	translation -= transform.basis.z * speed * delta
